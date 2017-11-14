d7e4f3baeaaa0f4a88ca9441c5488bd4a3f128dc
Andrew McLoughlin [el16a2m] <el16a2m@eentpc97.leeds.ac.uk>
2017-11-14 16:20:21 +0000

    Initial Commit

---
+++ DateInformationAppProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DateInformationAppProject
+//
+//  Created by Andrew McLoughlin [el16a2m] on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DateInformationAppProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DateInformationAppProject
+//
+//  Created by Andrew McLoughlin [el16a2m] on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
